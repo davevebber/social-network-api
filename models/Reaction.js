8780e4026d514b87e083c56342a2be7ebb452c83
dave vebber <davevebber@gmail.com>
2021-05-01 22:48:00 -0700

    user routes added

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,32 +1,33 @@
 const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
-    {
-      reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-      },
-      reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280
-      },
-      username: {
-        type: String,
-        required: true
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: timestamp => dateFormat(timestamp)
-      }
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
     },
-    {
-      toJSON: {
-        getters: true
-      },
-      id: false
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
     }
-  );
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
 
-  module.exports = reactionSchema
+module.exports = reactionSchema;
